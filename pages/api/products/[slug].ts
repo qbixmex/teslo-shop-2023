ae406e7cb8ce05d88bfae6025a5daed95578be7d
Daniel González Briseño <qbixmex@gmail.com>
2023-02-24 13:05:29 -0600

    get-product-backed

---
+++ pages/api/products/[slug].ts
@@ -0,0 +1,40 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { db } from '../../../database';
+import { Product } from '../../../models';
+import { IProduct } from '../../../interfaces';
+
+type Data =
+  | { message: string }
+  | IProduct;
+
+const handler = async (request: NextApiRequest, response: NextApiResponse<Data>) => {
+  const slug = request.query.slug as string;
+
+  switch (request.method) {
+    case 'GET':
+      return getProductBySlug(request, response, slug);
+    default:
+      return response.status(400).json({ message: 'Endpoint does not exist!' });
+  }
+};
+
+const getProductBySlug = async (
+  _request: NextApiRequest,
+  response: NextApiResponse<Data>,
+  slug: string,
+) => {
+  await db.connect();
+  const product = await Product
+    .findOne({ slug })
+    .select('-__v')
+    .lean();
+  await db.disconnect();
+ 
+  if (!product) {
+    return response.status(400).json({ message: `Product does not exist with Slug: "${slug}"` });
+  }
+
+  return response.status(200).json(product);
+};
+
+export default handler;
