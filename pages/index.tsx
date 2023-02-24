7be9a470b0266a3881f224662107fb6a535189d1
Daniel González Briseño <qbixmex@gmail.com>
2023-02-24 14:10:09 -0600

    swr-reack-hooks-for-data-fetching

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,23 @@
 import type { NextPage } from 'next'
 import { ProductList, ShopLayout } from '../components';
 import { Box, Typography } from '@mui/material';
-import { initialData } from '../database/products';
 
-const Home: NextPage = () => {
+import useSWR from 'swr';
+import { IProduct } from '../interfaces';
+
+const fetcher = async (...args: [key:string]): Promise<IProduct[]|undefined> => {
+  const response = await fetch(...args);
+  const data = await response.json() as IProduct[];
+  return data;
+};
+
+const HomePage: NextPage = () => {
+
+  const { data, error } = useSWR('/api/products', fetcher);
+
+  if (error) return <p>Failed to load</p>;
+  if (!data) return <p>Loading ...</p>;
+
   return (
     <ShopLayout
       title='Teslo Shop - Home'
@@ -14,9 +28,9 @@ const Home: NextPage = () => {
         <Typography variant='h1' component='h1'>Shop</Typography>
         <Typography variant='h2'>All Products</Typography>
       </Box>
-      <ProductList products={ initialData.products as any } />
+      <ProductList products={ data } />
     </ShopLayout>
   );
 };
 
-export default Home;
+export default HomePage;
