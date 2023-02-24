0ff4226771e2b151e81b0c2b89ced602891b7172
Daniel González Briseño <qbixmex@gmail.com>
2023-02-24 17:32:07 -0600

    custom-hook-global-config-swr

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,22 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { CssBaseline, ThemeProvider } from '@mui/material';
 import { lightTheme } from '../themes';
+import { SWRConfig } from 'swr';
 
 const MyApp = ({ Component, pageProps }: AppProps) =>  {
   return (
-    <ThemeProvider theme={ lightTheme }>
-      <CssBaseline />
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <SWRConfig value={{
+      fetcher: async (resource, init) => {
+        const response = await fetch(resource, init);
+        const data = await response.json();
+        return data;
+      }
+    }}>
+      <ThemeProvider theme={ lightTheme }>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </SWRConfig>
   );
 };
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,11 @@
 import type { NextPage } from 'next'
 import { ProductList, ShopLayout } from '../components';
 import { Box, Typography } from '@mui/material';
-
-import useSWR from 'swr';
-import { IProduct } from '../interfaces';
-
-const fetcher = async (...args: [key:string]): Promise<IProduct[]|undefined> => {
-  const response = await fetch(...args);
-  const data = await response.json() as IProduct[];
-  return data;
-};
+import { useProducts } from '../hooks';
 
 const HomePage: NextPage = () => {
 
-  const { data, error } = useSWR('/api/products', fetcher);
-
-  if (error) return <p>Failed to load</p>;
-  if (!data) return <p>Loading ...</p>;
+  const { products, isLoading } = useProducts('/products');
 
   return (
     <ShopLayout
@@ -28,7 +17,11 @@ const HomePage: NextPage = () => {
         <Typography variant='h1' component='h1'>Shop</Typography>
         <Typography variant='h2'>All Products</Typography>
       </Box>
-      <ProductList products={ data } />
+      {
+        isLoading
+          ? <h1>Loading</h1>
+          : <ProductList products={ products } />
+      }
     </ShopLayout>
   );
 };
