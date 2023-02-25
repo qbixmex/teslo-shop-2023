c6db0f1ba3e5a76cc975585d9b5a2be626a8c361
Daniel González Briseño <qbixmex@gmail.com>
2023-02-25 11:10:05 -0600

    user-experience-on-products-not-found
    Show all products in case there's no products by user's search terms

--- database/dbProducts.ts
+++ database/dbProducts.ts
@@ -22,11 +22,22 @@ export const getAllProductsSlugs = async (): Promise<productSlug[]> => {
   return slugs;
 };
 
-export const getProductByTerm = async (term: string): Promise<IProduct[]> => {
+export const getProductsByTerm = async (term: string): Promise<IProduct[]> => {
   term = term.toString().toLowerCase();
   await db.connect();
   const products = await Product
     .find({ $text: { $search: term } })
+    .select('title images price inStock slug')
+    .lean();
+  await db.disconnect();
+
+  return JSON.parse(JSON.stringify(products));
+};
+
+export const getAllProducts = async (): Promise<IProduct[]> => {
+  await db.connect();
+  const products = await Product
+    .find()
     .select('title images price inStock slug')
     .lean();
   await db.disconnect();
--- pages/search/[query].tsx
+++ pages/search/[query].tsx
@@ -5,18 +5,38 @@ import { ProductList } from '../../components';
 import { dbProducts } from '../../database';
 import { IProduct } from '../../interfaces/products';
 
-type Props = { products: IProduct[] };
+type Props = {
+  products: IProduct[];
+  foundProducts: boolean;
+  query: string;
+};
 
-const SearchPage: NextPage<Props> = ({ products }) => {
+const SearchPage: NextPage<Props> = ({ products, foundProducts, query }) => {
   return (
     <ShopLayout
-      title='xxxx Products'
-      pageDescription='xxxx products results'
+      title={`Teslo Shop - ${query} products`}
+      pageDescription={`Teslo Shop - ${query} products results`}
       robots='noindex, nofollow'
     >
       <Box mb={ 3 }>
-        <Typography variant='h1' component='h1'>Search Product</Typography>
-        <Typography variant='h2'>ABC Products</Typography>
+        <Typography variant='h1' component='h1' mb={2}>Search {query}</Typography>
+        <Typography variant='h2' mb={1}>
+        {
+          foundProducts
+            ? (
+              <>
+                Search Term:&nbsp;
+                <span className="blue bold">&quot;{ query }&quot;</span>
+              </>
+            )
+            : (
+              <>
+                There&apos;s no products with search term:&nbsp;
+                <span className="blue bold">&quot;{ query }&quot;</span>
+              </>
+            )
+        }
+        </Typography>
       </Box>
       <ProductList products={ products } />
     </ShopLayout>
@@ -35,12 +55,17 @@ export const getServerSideProps: GetServerSideProps = async ({params}) => {
     };
   }
 
-  let products = await dbProducts.getProductByTerm(query);
+  let products = await dbProducts.getProductsByTerm(query);
+  const foundProducts = (products.length > 0);
 
-  // TODO: Return other products if no results were found
+  if (!foundProducts) products = await dbProducts.getAllProducts();
 
   return {
-    props: { products }
+    props: {
+      products,
+      foundProducts,
+      query,
+    }
   }
 }
 
