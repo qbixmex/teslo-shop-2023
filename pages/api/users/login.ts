16e0abaa0f1620135f0b4b21ba44be603190c5c3
Daniel González Briseño <qbixmex@gmail.com>
2023-02-28 09:43:58 -0600

    rest-login

---
+++ pages/api/users/login.ts
@@ -0,0 +1,57 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import bcrypt from 'bcryptjs';
+import { db } from '../../../database';
+import { User } from '../../../models';
+
+type Data =
+  | { message: string; }
+  | {
+    token: string;
+    user: {
+      name: string;
+      email: string;
+      role: string;
+    }
+  };
+
+const handler = (
+  request: NextApiRequest,
+  response: NextApiResponse<Data>
+) => {
+  switch(request.method) {
+    case 'POST':
+      return loginUser(request, response);
+    default:
+      return response.status(400).json({
+        message: 'Endpoint does not exist!'
+      });
+  }
+};
+
+const loginUser = async (
+  request: NextApiRequest,
+  response: NextApiResponse<Data>
+) => {
+  const { email = '', password = '' } = request.body;
+
+  db.connect();
+  const user = await User.findOne({ email }).lean();
+  db.disconnect();
+
+  if (!user) {
+    return response.status(400).json({ message: 'Email or Password not valid! - EMAIL' });
+  }
+
+  if (!bcrypt.compareSync(password, user.password!)) {
+    return response.status(400).json({ message: 'Email or Password not valid! - PASSWORD' });
+  }
+
+  const { role, name } = user;
+
+  return response.status(200).json({
+    token: '',
+    user: { name, email, role },
+  });
+};
+
+export default handler;
