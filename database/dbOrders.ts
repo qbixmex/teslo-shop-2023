40025fdb585a90fcde5b624fa6ff6fbb5cbbb299
Daniel González Briseño <qbixmex@gmail.com>
2023-03-03 20:00:05 -0600

    user-order-history

--- database/dbOrders.ts
+++ database/dbOrders.ts
@@ -11,3 +11,11 @@ export const getOrderById = async (id: string): Promise<IOrder|null> => {
   if (!order) return null;
   return JSON.parse(JSON.stringify(order));
 };
+
+export const getOrderByUserId = async (userId: string): Promise<IOrder[]> => {
+  if (!isValidObjectId) return [];
+  await db.connect();
+  const orders = await Order.find({ user: userId }).lean();
+  await db.disconnect();
+  return JSON.parse(JSON.stringify(orders));
+};
--- pages/orders/history.tsx
+++ pages/orders/history.tsx
@@ -1,10 +1,16 @@
+import { GetServerSideProps, NextPage } from 'next'
+import { getSession } from 'next-auth/react';
 import NextLink from 'next/link';
+
 import { DataGrid, GridColDef } from '@mui/x-data-grid';
 import { Chip, Grid, Typography, Link } from '@mui/material';
-import { ShopLayout } from '../../components';
 import CreditCardOffIcon from '@mui/icons-material/CreditCardOffOutlined';
 import CreditScoreIcon from '@mui/icons-material/CreditScoreOutlined';
+
+import { getOrderByUserId } from '../../database/dbOrders';
+import { ShopLayout } from '../../components';
 import styles from './history.module.css';
+import { IOrder } from '../../interfaces';
 
 const columns: GridColDef[] = [
   { field: 'id', headerName: 'ID', width: 100 },
@@ -27,28 +33,34 @@ const columns: GridColDef[] = [
     description: 'Show information if order was placed',
     width: 120,
     renderCell: (params) => (
-      <NextLink href={`/orders/${params.row.id}`} passHref legacyBehavior>
-        <Link className={styles.link}>
-          Show Order
-        </Link>
+      <NextLink href={`/orders/${params.row.orderId}`} passHref legacyBehavior>
+        <Link className={styles.link}>Show Order</Link>
       </NextLink>
     ),
     sortable: false,
   },
-  { field: 'fullName', headerName: 'Full Name', width: 200 },
+  { field: 'firstName', headerName: 'First Name', width: 120 },
+  { field: 'lastName', headerName: 'Last Name', width: 170 },
+  { field: 'orderNumber', headerName: 'Order Number', width: 250 },
 ];
 
-const rows = [
-  { id: "x6PBfb4", paid: false, fullName: 'Home Simpson' },
-  { id: "x3P9fb5", paid: true, fullName: 'Charles Montgomery Burns' },
-  { id: "x8P7fbX", paid: false, fullName: 'Marge Simpson' },
-  { id: "b9P9fb7", paid: true, fullName: 'Chief Gorgory' },
-  { id: "mP5xb45", paid: false, fullName: 'Bart Simpson' },
-  { id: "iY5O3b4", paid: false, fullName: 'Barney Gumble' },
-  { id: "$6Po426", paid: true, fullName: 'Lisa Simpson' },
-];
+type Props = {
+  orders: IOrder[];
+};
 
-const HistoryPage = () => {
+const HistoryPage: NextPage<Props> = ({ orders }) => {
+
+  const rows = orders.map((order, index) => {
+    return {
+      id: index + 1,
+      orderId: order._id,
+      paid: order.isPaid,
+      firstName: order.shippingAddress.firstName,
+      lastName: order.shippingAddress.lastName,
+      orderNumber: order._id,
+    };
+  });
+
   return (
     <ShopLayout
       title="Teslo Shop - Order History"
@@ -60,7 +72,7 @@ const HistoryPage = () => {
         className={styles.title}
       >Order History</Typography>
 
-      <Grid container>
+      <Grid container className="fadeIn">
         <Grid item xs={12} className={styles.item}>
           <DataGrid
             rows={rows}
@@ -73,5 +85,27 @@ const HistoryPage = () => {
     </ShopLayout>
   );
 };
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+
+  const session: any = await getSession({ req: context.req });
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: '/auth/login?page=/orders/history',
+        permanent: false,
+      }
+    };
+  }
+
+  const orders = await getOrderByUserId(session.user.id);
+  
+  return {
+    props: {
+      orders
+    }
+  }
+}
 
 export default HistoryPage;
