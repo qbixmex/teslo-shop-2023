5e698a77759f97007aa52c22f9878a8ff35ed3b7
Daniel González Briseño <qbixmex@gmail.com>
2023-03-06 22:51:21 -0600

    paypal-o-auth-token

--- pages/api/orders/pay.ts
+++ pages/api/orders/pay.ts
@@ -1,7 +1,9 @@
+import axios from 'axios';
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 type Data =
   | { message: string }
+  | { token: string };
 
 const handler = (req: NextApiRequest, res: NextApiResponse<Data>) => {
   switch (req.method) {
@@ -12,8 +14,39 @@ const handler = (req: NextApiRequest, res: NextApiResponse<Data>) => {
   }
 }
 
-const payOrder = (req: NextApiRequest, res: NextApiResponse<Data>) => {
-  return res.status(201).json({ message: 'Created Order' });
+const getPaypalBearerToken = async (): Promise<string|null> => {
+  const PAYPAL_CLIENT = process.env.NEXT_PUBLIC_PAYPAL_CLIENT;
+  const PAYPAL_SECRET = process.env.PAYPAL_SECRET;
+
+  const base64Token = Buffer.from(`${ PAYPAL_CLIENT }:${ PAYPAL_SECRET }`, 'utf-8').toString('base64');
+
+  const body = new URLSearchParams('grant_type=client_credentials');
+
+  try {
+    const { data } = await axios.post(process.env.PAYPAL_OAUTH_URL ?? '', body, {
+      headers: {
+        'Authorization': `Basic ${ base64Token }`,
+        'Content-Type': 'application/x-www-form-urlencoded'
+      }
+    })
+    return data.access_token;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      console.log(error.response?.data);
+    } else {
+      console.log("Unknown Error!");
+      console.log(error);
+    }
+    return null;
+  }
+};
+
+const payOrder = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+  const paypalBearerToken = await getPaypalBearerToken();
+  if (!paypalBearerToken) {
+    return res.status(400).json({ message: 'Cannot confirm paypal token' });
+  }
+  return res.status(200).json({ token: paypalBearerToken });
 };
 
 export default handler;
