a3c1f1f19997ad702be9907bf83cd214875f562c
Daniel González Briseño <qbixmex@gmail.com>
2023-03-07 21:28:28 -0600

    middleware-validation-dashboard

--- middleware.ts
+++ middleware.ts
@@ -3,17 +3,44 @@ import type { NextRequest } from 'next/server'
 import { getToken } from 'next-auth/jwt';
 
 export async function middleware(request: NextRequest) {
-  const session = await getToken({
+  const session: any = await getToken({
     req: request,
     secret: process.env.NEXTAUTH_SECRET
   });
 
+  const validRoles = ['admin', 'super-user', 'SEO'];
+
   if (!session) {
+    if (request.nextUrl.pathname.startsWith('/api/admin')) {
+      return new NextResponse(JSON.stringify({ message: 'Not Authorized' }), {
+        status: 401,
+        headers: { 'content-type': 'application/json' }
+      });
+    }
+
     const requestedPage = request.nextUrl.pathname;
     const url = request.nextUrl.clone();
     url.pathname = '/auth/login';
     url.search = `page=${requestedPage}`;
+
     return NextResponse.redirect(url);
+  }
+
+  if (
+    request.nextUrl.pathname.startsWith('/api/admin') &&
+    !validRoles.includes(session.user.role)
+  ) {
+    return new NextResponse(JSON.stringify({ message: 'Not Authorized' }), {
+      status: 401,
+      headers: { 'content-type': 'application/json' }
+    });
+  }
+
+  if (
+    request.nextUrl.pathname.startsWith('/admin') &&
+    !validRoles.includes(session.user.role)
+  ) {
+    return NextResponse.redirect(new URL('/', request.url));
   }
 
   return NextResponse.next();
@@ -21,7 +48,8 @@ export async function middleware(request: NextRequest) {
 
 export const config = {
   matcher: [
-    '/checkout/address',
-    '/checkout/summary'
+    '/checkout/:path*',
+    '/admin/:path*',
+    '/api/admin/:path*',
   ],
 };
