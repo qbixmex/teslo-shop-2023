850447edb7c8cdc03bb593bd3a894bc6d0711b23
Daniel González Briseño <qbixmex@gmail.com>
2023-03-09 09:25:58 -0600

    update-product-backend

--- pages/api/admin/products/[id].ts
+++ pages/api/admin/products/[id].ts
@@ -14,6 +14,7 @@ const handler = (
     case 'GET':
       return getProduct(request, response);
     case 'PATCH':
+      return updateProduct(request, response);
     default:
       return response.status(400).json({ message: 'Unknown Request!' });
   }  
@@ -21,7 +22,7 @@ const handler = (
 
 const getProduct = async (
   request: NextApiRequest,
-  response: NextApiResponse
+  response: NextApiResponse<Data>
 ) => {
   const id = request.query.id as string;
 
@@ -30,7 +31,7 @@ const getProduct = async (
   }
 
   await db.connect();
-  const product = await Product.findById({ _id: id }).select('-__v').lean();
+  const product = await Product.findById(id).select('-__v').lean();
   await db.disconnect();
 
   if (!product) {
@@ -39,6 +40,47 @@ const getProduct = async (
   }
 
   return response.status(200).json(product);
+};
+
+const updateProduct = async (
+  request: NextApiRequest,
+  response: NextApiResponse<Data>
+) => {
+  const id = request.query.id as string;
+  const { images = [] } = request.body as IProduct;
+
+  if (!isValidObjectId(id)) {
+    return response.status(400).json({ message: `${id} - Is not a valid "Mongo ID"` });
+  }
+
+  if (images.length < 2) {
+    return response.status(400).json({ message: "You must include at least 2 images!" });
+  }
+
+  // TODO: Handle images
+
+  try {
+    await db.connect();
+
+    const product = await Product.findOneAndUpdate({ _id: id }, request.body, { new: true });
+
+    if (!product) {
+      await db.disconnect();
+      return response.status(404)
+        .json({ message: `Order with ID: "${id}" not found!` });
+    }
+
+    // TODO: Delete pictures (Cloudinary)
+
+    await db.disconnect();
+
+    return response.status(200).json(product);
+
+  } catch (error) {
+    console.log(error);
+    await db.disconnect();
+    return response.status(400).json({ message: 'Check console error logs!' });
+  }  
 };
 
 export default handler;
